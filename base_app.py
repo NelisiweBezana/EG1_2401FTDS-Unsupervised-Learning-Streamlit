36fe34831a04cbf07513e9a20eac7c412b76f645
Nelisiwe Bezana <nelisiwebezana@gmail.com>
2024-07-18 13:44:52 +0200

    Editing markdowns

--- base_app.py
+++ base_app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 
 def main():
-    st.image("visuals/assets/background_image.png", use_column_width=True)
-
-    st.title("Movie Recommendation System")
+    st.title("Anime Recommendation System")
 
     st.header("Select an algorithm")
     algorithm = st.radio("Select an algorithm:",
                          ('Content Based Filtering', 'Collaborative Based Filtering'))
 
-    st.header("Enter Your Three Favorite Movies")
+    st.header("Enter Your Three Favorite Anime")
     first_option = st.text_input("First Option")
     second_option = st.text_input("Second Option")
     third_option = st.text_input("Third Option")
@@ -19,6 +17,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
 
 
