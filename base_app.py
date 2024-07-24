021544e4f164af550c1628e372ec9389d896256f
Nelisiwe Bezana <nelisiwebezana@gmail.com>
2024-07-24 16:41:09 +0200

    Updating theme

--- base_app.py
+++ base_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Anime Recommender", page_icon=":shark:", layout="centered", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Anime Recommender", 
+                   page_icon=":shark:", 
+                   layout="centered", 
+                   initial_sidebar_state="expanded",
+                   )
 
 def display_team_member(image, name, surname, email, github, linkedin):
     st.image(image, width=120)
